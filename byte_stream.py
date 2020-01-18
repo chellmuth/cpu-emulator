a4aa96bb9e3cd62d5e1c7fb92051c37ba9b36152
Chris Hellmuth <chellmuth@gmail.com>
2020-01-18 13:37:33 -0800

    Disassemble source and dest

--- byte_stream.py
+++ byte_stream.py
@@ -28,3 +28,34 @@ class ByteStream:
                 byte_list.append(int(byte_str, 2))
 
         return byte_list
+
+
+class BitStream:
+    def __init__(self, filename):
+        real_bytes = open(filename, "rb").read()
+
+        # from bytes -> list of binary-encoding strings [ "11010", "0111", ... ]
+        bits_strs = [
+            bin(byte)[2:] for byte in real_bytes
+        ]
+
+        # left-pad up to a full byte "11010" -> "00011010"
+        byte_strs = [ util.pad(bits_str, 8) for bits_str in bits_strs ]
+
+        # one big sequence of binary
+        self.bin_str = "".join(byte_strs)
+
+
+    def read_int(self, bit_count):
+        assert(bit_count <= 7)
+
+        bit_str = self.bin_str[:bit_count]
+        self.bin_str = self.bin_str[bit_count:]
+
+        return int(bit_str, 2),
+
+    def read_str(self, bit_count):
+        bit_str = self.bin_str[:bit_count]
+        self.bin_str = self.bin_str[bit_count:]
+
+        return bit_str,
--- disassembler.py
+++ disassembler.py
@@ -1,12 +1,12 @@
 import sys
 
-from byte_stream import ByteStream
+from byte_stream import BitStream
 
 def disassemble(filename):
     disassembled_lines = []
 
-    stream = ByteStream(filename)
-    op_code, = stream.read(1)
+    stream = BitStream(filename)
+    op_code, = stream.read_int(7)
 
     if op_code & 0b1111 == 0b0000:
         print("ADD")
@@ -15,8 +15,27 @@ def disassemble(filename):
 
     if op_code & 0b1110000 == 0b0100000:
         print("REGISTER")
+
+        source, = stream.read_int(4)
+        dest, = stream.read_int(4)
+        skip, = stream.read_int(6)
+
+        assert(skip == 0)
+
+        print("SOURCE:", source)
+        print("DEST:", dest)
+
     elif op_code & 0b1110000 == 0b0110000:
         print("CONSTANT")
+
+        dest, = stream.read_int(4)
+        source, = stream.read_str(7)
+        skip, = stream.read_int(3)
+
+        assert(skip == 0)
+
+        print("SOURCE:", source)
+        print("DEST:", dest)
 
     print(disassembled_lines)
 
