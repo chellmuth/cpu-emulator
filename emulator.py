9eba30db0049403024bb1c7a2a899e2f9d5a20f0
Chris Hellmuth <chellmuth@gmail.com>
2020-01-18 00:24:56 -0800

    Emulator can add

--- emulator.py
+++ emulator.py
@@ -1,12 +1,31 @@
-from core import Flag, Register
+from core import Byte, Flag, Register
+from ops.add import add
 
 class Machine:
     def __init__(self):
-        self.registers = [ 0 for _ in Register ]
-        self.flags = [ 0 for _ in Flag ]
+        self.registers = [ Byte(0b0) for _ in Register ]
+        self.flags = [ Byte(0b0) for _ in Flag ]
 
-    def add_register(self, src, dest):
-        pass
+    def __repr__(self):
+        return str(self.registers)
+
+    def add_from_register(self, dest, src):
+        result, flags = add(self.registers[src], self.registers[dest])
+
+        self.registers[dest] = result
+        self.flags = flags
+
+    def add_from_constant(self, dest, constant):
+        result, flags = add(self.registers[dest], constant)
+
+        self.registers[dest] = result
+        self.flags = flags
 
 if __name__ == "__main__":
     m = Machine()
+
+    m.add_from_constant(Register.rc, Byte(0b1))
+    m.add_from_constant(Register.rd, Byte(0b1000))
+    m.add_from_register(Register.rd, Register.rc)
+
+    print(m)
