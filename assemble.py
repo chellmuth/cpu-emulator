4fdf9f51e7f7635af7f8b55347cb7e7fbd36d9f3
Chris Hellmuth <chellmuth@gmail.com>
2020-01-18 10:39:16 -0800

    Correctly assemble an add instruction

--- assemble.py
+++ assemble.py
@@ -2,15 +2,17 @@ import sys
 
 from core import Register
 
+def pad(bits_str, length):
+    return ("0" * length + bits_str)[-length:]
+
 def is_register(arg):
     return arg.startswith("r")
 
 def get_register_id(register_name):
-    # NEEDS TO BE FORMATTED!
-    return bin(Register[register_name])[2:]
+    return pad(bin(Register[register_name])[2:], 4)
 
 def get_const_binary(int_value):
-    return "111"
+    return pad("111", 28)
 
 def assemble(filename):
     assembled_lines = []
@@ -26,14 +28,17 @@ def assemble(filename):
             if is_register(arg2):
                 type_code = "010"
                 source = get_register_id(arg2)
-            else:        
+                right_padding = "0" * 6
+            else:
                 type_code = "011"
                 source = get_const_binary(arg2)
+                right_padding = "0" * 3
 
             assembled_lines.append(
-                type_code + "0000" + dest + source
+                type_code + "0000" + dest + source + right_padding
             )
 
     print(assembled_lines)
+
 if __name__ == "__main__":
     assemble(sys.argv[1])
