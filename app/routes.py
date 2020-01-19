940539126856624dae82b515c4a81305bcc1bacd
Chris Hellmuth <chellmuth@gmail.com>
2020-01-19 15:42:15 -0800

    Show fake register values

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
+from dataclasses import dataclass
+
 from flask import render_template
 
 from app import app
 
+@dataclass
+class RegisterView:
+    name: str
+    value: str
+
 @app.route('/')
 def hello_world():
-    return render_template("emulator.html")
+    registers = [
+        RegisterView("ra", "0x01"),
+        RegisterView("rb", "0x0112"),
+    ]
+    return render_template("emulator.html", registers=registers)
