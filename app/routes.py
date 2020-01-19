682d7587e3fcc3c60ecf7ed2e39448eab940ae14
Chris Hellmuth <chellmuth@gmail.com>
2020-01-19 15:52:00 -0800

    Show a mock instruction

--- app/routes.py
+++ app/routes.py
@@ -9,10 +9,21 @@ class RegisterView:
     name: str
     value: str
 
+program_file = "./program.txt"
+program = open(program_file).readlines()
+print(program)
+
 @app.route('/')
 def hello_world():
+    instruction = program.pop(0).strip()
+
     registers = [
         RegisterView("ra", "0x01"),
         RegisterView("rb", "0x0112"),
     ]
-    return render_template("emulator.html", registers=registers)
+
+    return render_template(
+        "emulator.html",
+        instruction=instruction,
+        registers=registers,
+    )
