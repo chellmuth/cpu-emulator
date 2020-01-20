a32949f1af35cdaa3ac4740386aa89a9d668f94b
Chris Hellmuth <chellmuth@gmail.com>
2020-01-20 10:08:51 -0800

    (Untested) Implement add instruction

--- ops/add.py
+++ ops/add.py
@@ -7,14 +7,14 @@ class AddRegisterInstruction(instruction_base.Type1RegisterInstruction):
         super().__init__("ADD", dest_register, source_register)
 
     def run(self, machine):
-        result, flags = add(
-            machine.registers[self.dest_register].low_byte(),
-            machine.registers[self.source_register].low_byte()
+        result, flags = add_word(
+            machine.registers[self.dest_register],
+            machine.registers[self.source_register]
         )
 
         return MachineUpdate(
             registers={
-                self.dest_register: Word(result, Byte(0b0), Byte(0b0), Byte(0b0))
+                self.dest_register: result,
             },
             flags=flags
         )
@@ -24,17 +24,40 @@ class AddConstantInstruction(instruction_base.Type1ConstantInstruction):
         super().__init__("ADD", dest_register, source_word)
 
     def run(self, machine):
-        result, flags = add(
-            machine.registers[self.dest_register].low_byte(),
-            self.source_word.low_byte()
+        result, flags = add_word(
+            machine.registers[self.dest_register],
+            self.source_word
         )
 
         return MachineUpdate(
             registers={
-                self.dest_register: Word(result, Byte(0b0), Byte(0b0), Byte(0b0))
+                self.dest_register: result,
             },
             flags=flags
         )
+
+def add_word(word1, word2):
+    flags = set()
+
+    int_sum = word1.int_value() + word2.int_value()
+
+    if int_sum == 0:
+        flags.add(Flag.ZF)
+
+    sign_bit = 1 << 23
+    overflow_bit = 1 << 24
+
+    if int_sum & sign_bit:
+        flags.add(Flag.SF)
+
+    if int_sum & overflow_bit:
+        flags.add(Flag.CF)
+
+
+    if not ((word1.int_value() & sign_bit) ^ (word2.int_value() & sign_bit)):
+        flags.add(Flag.OF)
+
+    return Word.from_int(int_sum), flags
 
 def add(byte1, byte2):
     flags = set()
