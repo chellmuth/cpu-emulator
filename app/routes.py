2d6931b3600d0b054e53a8726a67ec2eeb2e5032
Chris Hellmuth <chellmuth@gmail.com>
2020-01-19 20:06:54 -0800

    Show real machine registers

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from flask import render_template
 
 from app import app
+from emulator import Machine
 
 @dataclass
 class RegisterView:
@@ -13,13 +14,15 @@ program_file = "./program.txt"
 program = open(program_file).readlines()
 print(program)
 
+machine = Machine()
+
 @app.route('/')
 def hello_world():
     instruction = program.pop(0).strip()
 
     registers = [
-        RegisterView("ra", "0x01"),
-        RegisterView("rb", "0x0112"),
+        RegisterView("ra", machine.registers[0].hex_str()),
+        RegisterView("rb", machine.registers[1].hex_str()),
     ]
 
     return render_template(
