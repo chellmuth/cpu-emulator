989e1086da90ac3f83d89f81612ad0472423c7ce
Chris Hellmuth <chellmuth@gmail.com>
2020-01-19 21:07:01 -0800

    Move add implementation

--- instruction.py
+++ instruction.py
@@ -1,12 +1,10 @@
 import instruction_base
+import ops.add
 import ops.bitwise_or
-from core import Word, Byte
-from emulator import MachineUpdate
-from ops.add import add
 
 def type1_register_factory(op_name, source_register, dest_register):
     cls_lookup = {
-        "ADD": AddRegisterInstruction,
+        "ADD": ops.add.AddRegisterInstruction,
         "ORR": ops.bitwise_or.BitwiseOrRegisterInstruction,
     }
 
@@ -21,7 +19,7 @@ def type1_register_factory(op_name, source_register, dest_register):
 
 def type1_constant_factory(op_name, dest_register, source_word):
     cls_lookup = {
-        "ADD": AddConstantInstruction,
+        "ADD": ops.add.AddConstantInstruction,
         "ORR": ops.bitwise_or.BitwiseOrConstantInstruction,
     }
 
@@ -59,37 +57,3 @@ class NopInstruction(instruction_base.Instruction):
 class RetInstruction(instruction_base.Instruction):
     def human(self):
         return "RET"
-
-class AddRegisterInstruction(instruction_base.Type1RegisterInstruction):
-    def __init__(self, dest_register, source_register):
-        super().__init__("ADD", dest_register, source_register)
-
-    def run(self, machine):
-        result, flags = add(
-            machine.registers[self.dest_register].low_byte(),
-            machine.registers[self.source_register].low_byte()
-        )
-
-        return MachineUpdate(
-            registers={
-                self.dest_register: Word(result, Byte(0b0), Byte(0b0), Byte(0b0))
-            },
-            flags=flags
-        )
-
-class AddConstantInstruction(instruction_base.Type1ConstantInstruction):
-    def __init__(self, dest_register, source_word):
-        super().__init__("ADD", dest_register, source_word)
-
-    def run(self, machine):
-        result, flags = add(
-            machine.registers[self.dest_register].low_byte(),
-            self.source_word.low_byte()
-        )
-
-        return MachineUpdate(
-            registers={
-                self.dest_register: Word(result, Byte(0b0), Byte(0b0), Byte(0b0))
-            },
-            flags=flags
-        )
--- ops/add.py
+++ ops/add.py
@@ -1,4 +1,40 @@
-from core import Byte, Flag
+import instruction_base
+from core import Byte, Flag, Word
+from emulator import MachineUpdate
+
+class AddRegisterInstruction(instruction_base.Type1RegisterInstruction):
+    def __init__(self, dest_register, source_register):
+        super().__init__("ADD", dest_register, source_register)
+
+    def run(self, machine):
+        result, flags = add(
+            machine.registers[self.dest_register].low_byte(),
+            machine.registers[self.source_register].low_byte()
+        )
+
+        return MachineUpdate(
+            registers={
+                self.dest_register: Word(result, Byte(0b0), Byte(0b0), Byte(0b0))
+            },
+            flags=flags
+        )
+
+class AddConstantInstruction(instruction_base.Type1ConstantInstruction):
+    def __init__(self, dest_register, source_word):
+        super().__init__("ADD", dest_register, source_word)
+
+    def run(self, machine):
+        result, flags = add(
+            machine.registers[self.dest_register].low_byte(),
+            self.source_word.low_byte()
+        )
+
+        return MachineUpdate(
+            registers={
+                self.dest_register: Word(result, Byte(0b0), Byte(0b0), Byte(0b0))
+            },
+            flags=flags
+        )
 
 def add(byte1, byte2):
     flags = set()
